5c8b454bbd5a20aab7c260c45be3e976e12b5552
Mohammed gamal <mo.arabie83@gmail.com>
2022-08-13 02:22:45 +0200

    job table construction 1

--- job/models.py
+++ job/models.py
@@ -1,3 +1,20 @@
+from pickle import TRUE
 from django.db import models
 
 # Create your models here.
+
+JOP_TYPE=(
+    ('FULL TIME','FULL TIME'),
+    ('PART TIME','PART TIME')
+)
+
+class Job (models.Model):
+    title=models.CharField(max_length=50)
+    jopType=models.CharField( max_length=50,choices=JOP_TYPE)
+    discribtion=models.TextField(max_length=1500)
+    published_at=models.DateTimeField(auto_now=TRUE)
+    vacancy=models.IntegerField(default=1)
+    salary=models.IntegerField(default=0)
+    experience=models.IntegerField(default=1)
+    def __str__(self) :
+        return self.title
--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #myapp
+    'job',
+    'blog',
+    'contact',
+    'home',
+    'accounts',
 ]
 
 MIDDLEWARE = [
