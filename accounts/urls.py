47295e8cd79463753085780dd1c7ea9c76655291
Mohammed gamal <mo.arabie83@gmail.com>
2022-08-27 21:45:46 +0200

    Sign up adding to nav bar

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from .views import  profile_edit, signup,profile
 app_name="accounts"
 urlpatterns = [
     
-    path('signup',signup , name="job_list"),
+    path('signup',signup , name="signup"),
     path('profile',profile , name="profile"),
     path('profile/',profile , name="profile"),
     path('profile/edit',profile_edit , name="profile_edit"),
